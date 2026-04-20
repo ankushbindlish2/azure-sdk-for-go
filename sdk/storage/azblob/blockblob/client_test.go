1768c1f7e68aea2453ee5598f970184c5273e538
Sourav Gupta <98318303+souravgupta-msft@users.noreply.github.com>
2026-04-20 15:54:12 +0530

    Object-Level Smart Access Tier  (#26595)

--- sdk/storage/azblob/blockblob/client_test.go
+++ sdk/storage/azblob/blockblob/client_test.go
@@ -1075,6 +1075,76 @@ func (s *BlockBlobUnrecordedTestsSuite) TestPutBlobFromURL() {
 	_require.Equal(destBuffer, sourceData)
 }
 
+func (s *BlockBlobUnrecordedTestsSuite) TestPutBlobFromURLSmartAccessTier() {
+	// Smart access tier is currently in public preview and not GA yet, skipping this test for now.
+	s.T().Skip("Smart access tier is in public preview and not GA yet")
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerClient, _, destBlob, srcBlobURLWithSAS, _ := setUpPutBlobFromURLTest(testName, _require, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	tier := blob.AccessTierSmart
+	pbResp, err := destBlob.UploadBlobFromURL(context.Background(), srcBlobURLWithSAS, &blockblob.UploadBlobFromURLOptions{
+		Tier: &tier,
+	})
+	_require.NotNil(pbResp)
+	_require.NoError(err)
+
+	resp, err := destBlob.GetProperties(context.Background(), nil)
+	_require.NoError(err)
+	_require.Equal(*resp.AccessTier, string(blob.AccessTierSmart))
+}
+
+func (s *BlockBlobUnrecordedTestsSuite) TestCopyFromURLSmartAccessTier() {
+	// Smart access tier is currently in public preview and not GA yet, skipping this test for now.
+	s.T().Skip("Smart access tier is in public preview and not GA yet")
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := testcommon.CreateNewContainer(context.Background(), _require, containerName, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	contentSize := 4 * 1024 // 4KB
+	contentReader, _ := testcommon.GetDataAndReader(testName, contentSize)
+
+	srcBlob := containerClient.NewBlockBlobClient(testcommon.GenerateBlobName(testName))
+	_, err = srcBlob.Upload(context.Background(), streaming.NopCloser(contentReader), nil)
+	_require.NoError(err)
+
+	credential, err := testcommon.GetGenericSharedKeyCredential(testcommon.TestAccountDefault)
+	_require.NoError(err)
+
+	sasQueryParams, err := sas.AccountSignatureValues{
+		Protocol:      sas.ProtocolHTTPS,
+		ExpiryTime:    time.Now().UTC().Add(48 * time.Hour),
+		Permissions:   to.Ptr(sas.AccountPermissions{Read: true, List: true}).String(),
+		ResourceTypes: to.Ptr(sas.AccountResourceTypes{Container: true, Object: true}).String(),
+	}.SignWithSharedKey(credential)
+	_require.NoError(err)
+
+	srcBlobParts, _ := blob.ParseURL(srcBlob.URL())
+	srcBlobParts.SAS = sasQueryParams
+	srcBlobURLWithSAS := srcBlobParts.String()
+
+	destBlob := containerClient.NewBlockBlobClient("dest" + testcommon.GenerateBlobName(testName))
+	tier := blob.AccessTierSmart
+	resp, err := destBlob.CopyFromURL(context.Background(), srcBlobURLWithSAS, &blob.CopyFromURLOptions{
+		Tier: &tier,
+	})
+	_require.NoError(err)
+	_require.Equal(*resp.CopyStatus, "success")
+
+	destBlobPropResp, err := destBlob.GetProperties(context.Background(), nil)
+	_require.NoError(err)
+	_require.Equal(*destBlobPropResp.AccessTier, string(blob.AccessTierSmart))
+}
+
 func (s *BlockBlobUnrecordedTestsSuite) TestPutBlobFromURLWithCopySourceTagsDefault() {
 	_require := require.New(s.T())
 	testName := s.T().Name()
@@ -3163,6 +3233,290 @@ func (s *BlockBlobRecordedTestsSuite) TestBlobSetTierAllTiersOnBlockBlob() {
 	setAndCheckBlockBlobTier(_require, bbClient, blob.AccessTierCool)
 	setAndCheckBlockBlobTier(_require, bbClient, blob.AccessTierArchive)
 
+}
+
+func (s *BlockBlobRecordedTestsSuite) TestSetTierSmart() {
+	// Smart access tier is currently in public preview and not GA yet. Only run in playback mode.
+	if recording.GetRecordMode() != recording.PlaybackMode {
+		s.T().Skip("Smart access tier is in public preview and not GA yet")
+	}
+
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := testcommon.CreateNewContainer(context.Background(), _require, containerName, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	bbName := testcommon.GenerateBlobName(testName)
+	bbClient := testcommon.CreateNewBlockBlob(context.Background(), _require, bbName, containerClient)
+
+	// Set tier to Smart
+	_, err = bbClient.SetTier(context.Background(), blob.AccessTierSmart, nil)
+	_require.NoError(err)
+
+	// Verify via GetProperties
+	getResp, err := bbClient.GetProperties(context.Background(), nil)
+	_require.NoError(err)
+	_require.Equal(*getResp.AccessTier, string(blob.AccessTierSmart))
+}
+
+func (s *BlockBlobRecordedTestsSuite) TestGetPropertiesSmartAccessTierHeader() {
+	// Smart access tier is currently in public preview and not GA yet. Only run in playback mode.
+	if recording.GetRecordMode() != recording.PlaybackMode {
+		s.T().Skip("Smart access tier is in public preview and not GA yet")
+	}
+
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := testcommon.CreateNewContainer(context.Background(), _require, containerName, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	bbName := testcommon.GenerateBlobName(testName)
+	bbClient := testcommon.CreateNewBlockBlob(context.Background(), _require, bbName, containerClient)
+
+	// Set tier to Smart
+	_, err = bbClient.SetTier(context.Background(), blob.AccessTierSmart, nil)
+	_require.NoError(err)
+
+	// Verify x-ms-smart-access-tier is returned in GetProperties response.
+	// This header is only returned for blobs in Smart tier and indicates the
+	// current sub-tier (Hot, Cool, or Cold) that data is placed in.
+	getResp, err := bbClient.GetProperties(context.Background(), nil)
+	_require.NoError(err)
+	_require.Equal(*getResp.AccessTier, string(blob.AccessTierSmart))
+	_require.Equal(*getResp.SmartAccessTier, string(blob.AccessTierHot)) // SmartAccessTier should default to Hot
+}
+
+func (s *BlockBlobRecordedTestsSuite) TestSetTierSmartListBlobs() {
+	// Smart access tier is currently in public preview and not GA yet. Only run in playback mode.
+	if recording.GetRecordMode() != recording.PlaybackMode {
+		s.T().Skip("Smart access tier is in public preview and not GA yet")
+	}
+
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := testcommon.CreateNewContainer(context.Background(), _require, containerName, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	bbName1 := "smartblob1" + testcommon.GenerateBlobName(testName)
+	bbClient1 := testcommon.CreateNewBlockBlob(context.Background(), _require, bbName1, containerClient)
+	bbName2 := "smartblob2" + testcommon.GenerateBlobName(testName)
+	bbClient2 := testcommon.CreateNewBlockBlob(context.Background(), _require, bbName2, containerClient)
+
+	// Set tier to Smart
+	_, err = bbClient1.SetTier(context.Background(), blob.AccessTierSmart, nil)
+	_require.NoError(err)
+	_, err = bbClient2.SetTier(context.Background(), blob.AccessTierSmart, nil)
+	_require.NoError(err)
+
+	// Verify via ListBlobs
+	pager := containerClient.NewListBlobsFlatPager(nil)
+	var blobs []*container.BlobItem
+	for pager.More() {
+		resp, err := pager.NextPage(context.Background())
+		_require.NoError(err)
+		blobs = append(blobs, resp.Segment.BlobItems...)
+	}
+	_require.GreaterOrEqual(len(blobs), 2)
+	for _, b := range blobs {
+		_require.Equal(*b.Properties.AccessTier, blob.AccessTierSmart)
+	}
+}
+
+func (s *BlockBlobRecordedTestsSuite) TestSetTierSmartRehydrate() {
+	// Smart access tier is currently in public preview and not GA yet. Only run in playback mode.
+	if recording.GetRecordMode() != recording.PlaybackMode {
+		s.T().Skip("Smart access tier is in public preview and not GA yet")
+	}
+
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := testcommon.CreateNewContainer(context.Background(), _require, containerName, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	bbName := testcommon.GenerateBlobName(testName)
+	bbClient := testcommon.CreateNewBlockBlob(context.Background(), _require, bbName, containerClient)
+
+	// Archive the blob first
+	_, err = bbClient.SetTier(context.Background(), blob.AccessTierArchive, nil)
+	_require.NoError(err)
+
+	// Rehydrate to Smart
+	rehydratePriority := blob.RehydratePriorityHigh
+	_, err = bbClient.SetTier(context.Background(), blob.AccessTierSmart, &blob.SetTierOptions{
+		RehydratePriority: &rehydratePriority,
+	})
+	_require.NoError(err)
+
+	// Verify ArchiveStatus is rehydrate-pending-to-smart
+	getResp, err := bbClient.GetProperties(context.Background(), nil)
+	_require.NoError(err)
+	_require.Equal(*getResp.ArchiveStatus, string(blob.ArchiveStatusRehydratePendingToSmart))
+}
+
+func (s *BlockBlobRecordedTestsSuite) TestSetTierSmartRehydrateListBlobs() {
+	// Smart access tier is currently in public preview and not GA yet. Only run in playback mode.
+	if recording.GetRecordMode() != recording.PlaybackMode {
+		s.T().Skip("Smart access tier is in public preview and not GA yet")
+	}
+
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := testcommon.CreateNewContainer(context.Background(), _require, containerName, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	bbName1 := "smartblob1" + testcommon.GenerateBlobName(testName)
+	bbClient1 := testcommon.CreateNewBlockBlob(context.Background(), _require, bbName1, containerClient)
+	bbName2 := "smartblob2" + testcommon.GenerateBlobName(testName)
+	bbClient2 := testcommon.CreateNewBlockBlob(context.Background(), _require, bbName2, containerClient)
+
+	// Archive both blobs
+	_, err = bbClient1.SetTier(context.Background(), blob.AccessTierArchive, nil)
+	_require.NoError(err)
+	_, err = bbClient2.SetTier(context.Background(), blob.AccessTierArchive, nil)
+	_require.NoError(err)
+
+	// Rehydrate both to Smart
+	rehydratePriority := blob.RehydratePriorityHigh
+	_, err = bbClient1.SetTier(context.Background(), blob.AccessTierSmart, &blob.SetTierOptions{
+		RehydratePriority: &rehydratePriority,
+	})
+	_require.NoError(err)
+
+	_, err = bbClient2.SetTier(context.Background(), blob.AccessTierSmart, &blob.SetTierOptions{
+		RehydratePriority: &rehydratePriority,
+	})
+	_require.NoError(err)
+
+	// Verify via ListBlobs
+	pager := containerClient.NewListBlobsFlatPager(nil)
+	var blobs []*container.BlobItem
+	for pager.More() {
+		resp, err := pager.NextPage(context.Background())
+		_require.NoError(err)
+		blobs = append(blobs, resp.Segment.BlobItems...)
+	}
+
+	_require.GreaterOrEqual(len(blobs), 2)
+	for _, b := range blobs {
+		_require.Equal(*b.Properties.ArchiveStatus, blob.ArchiveStatusRehydratePendingToSmart)
+	}
+}
+
+func (s *BlockBlobRecordedTestsSuite) TestSetTierOnBlobUploadSmart() {
+	// Smart access tier is currently in public preview and not GA yet. Only run in playback mode.
+	if recording.GetRecordMode() != recording.PlaybackMode {
+		s.T().Skip("Smart access tier is in public preview and not GA yet")
+	}
+
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := testcommon.CreateNewContainer(context.Background(), _require, containerName, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	blobName := testcommon.GenerateBlobName(testName)
+	bbClient := testcommon.GetBlockBlobClient(blobName, containerClient)
+
+	tier := blob.AccessTierSmart
+	uploadBlockBlobOptions := blockblob.UploadOptions{
+		Tier: &tier,
+	}
+
+	_, err = bbClient.Upload(context.Background(), streaming.NopCloser(strings.NewReader(testcommon.BlockBlobDefaultData)), &uploadBlockBlobOptions)
+	_require.NoError(err)
+
+	resp, err := bbClient.GetProperties(context.Background(), nil)
+	_require.NoError(err)
+	_require.Equal(*resp.AccessTier, string(blob.AccessTierSmart))
+}
+
+func (s *BlockBlobRecordedTestsSuite) TestBlobSetTierOnCommitSmart() {
+	// Smart access tier is currently in public preview and not GA yet. Only run in playback mode.
+	if recording.GetRecordMode() != recording.PlaybackMode {
+		s.T().Skip("Smart access tier is in public preview and not GA yet")
+	}
+
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerName := "test" + testcommon.GenerateContainerName(testName)
+	containerClient := testcommon.CreateNewContainer(context.Background(), _require, containerName, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	blobName := testcommon.GenerateBlobName(testName)
+	bbClient := testcommon.GetBlockBlobClient(blobName, containerClient)
+
+	blockID := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%6d", 0)))
+	_, err = bbClient.StageBlock(context.Background(), blockID, streaming.NopCloser(strings.NewReader(testcommon.BlockBlobDefaultData)), nil)
+	_require.NoError(err)
+
+	tier := blob.AccessTierSmart
+	_, err = bbClient.CommitBlockList(context.Background(), []string{blockID}, &blockblob.CommitBlockListOptions{
+		Tier: &tier,
+	})
+	_require.NoError(err)
+
+	getResp, err := bbClient.GetProperties(context.Background(), nil)
+	_require.NoError(err)
+	_require.Equal(*getResp.AccessTier, string(blob.AccessTierSmart))
+}
+
+func (s *BlockBlobRecordedTestsSuite) TestStartCopyFromURLSmartAccessTier() {
+	// Smart access tier is currently in public preview and not GA yet. Only run in playback mode.
+	if recording.GetRecordMode() != recording.PlaybackMode {
+		s.T().Skip("Smart access tier is in public preview and not GA yet")
+	}
+
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := testcommon.CreateNewContainer(context.Background(), _require, containerName, svcClient)
+	defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+
+	sourceBlobName := testcommon.GenerateBlobName(testName)
+	sourceBBClient := testcommon.CreateNewBlockBlob(context.Background(), _require, sourceBlobName, containerClient)
+
+	destBlobName := "dest" + testcommon.GenerateBlobName(testName)
+	destBBClient := testcommon.GetBlockBlobClient(destBlobName, containerClient)
+
+	tier := blob.AccessTierSmart
+	_, err = destBBClient.StartCopyFromURL(context.Background(), sourceBBClient.URL(), &blob.StartCopyFromURLOptions{
+		Tier: &tier,
+	})
+	_require.NoError(err)
+
+	// Wait briefly and check tier
+	getResp, err := destBBClient.GetProperties(context.Background(), nil)
+	_require.NoError(err)
+	_require.Equal(*getResp.AccessTier, string(blob.AccessTierSmart))
 }
 
 func (s *BlockBlobRecordedTestsSuite) TestBlockBlobGetPropertiesUsingVID() {
